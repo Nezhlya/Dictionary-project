60bc738b536b21285c134e25d8a514331e94d4f3
nezhlya <eminejla@gmail.com>
2023-07-24 10:14:52 +0200

    synonyms added

--- my-app/src/Meaning.js
+++ my-app/src/Meaning.js
@@ -1,22 +1,27 @@
 import React from "react";
-
+import Synonyms from "./Synonyms.js";
 export default function Meaning(props) {
   return (
     <div>
+            <p>{props.meaning.definitions[0].definition}</p>
+
+      <p><em>{props.meaning.definitions[0].example}</em></p>
+
       <h3> {props.meaning.partOfSpeech} </h3>
       {props.meaning.definitions.map(function(definition, index){
         return(
             <div>
                 <p>
+                  <strong>Definition: </strong>
                     {definition.definition}
                     <br />
+                    
                     <em>{definition.example}</em>
+                    <Synonyms synonyms={definition.synonyms} />
                 </p>
             </div>
         )
       })}
-      <p>{props.meaning.definitions[0].definition}</p>
-      <p>{props.meaning.definitions[0].example}</p>
     </div>
   );
 }
